39c8e2c4cd625d0eb24163c6379f930a7c194fad
Martin Svoboda <martin.svoboda@ad.is4u.cz>
2022-03-02 09:50:45 +0100

    Icon color fixed in dark mode

--- app/src/main/java/com/example/kaloricketabulkylite/ui/components/Button.kt
+++ app/src/main/java/com/example/kaloricketabulkylite/ui/components/Button.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import com.example.kaloricketabulkylite.ui.theme.KalorickeTabulkyLiteTheme
 
 @Composable
 fun NavigationIconTopAppBarKalorickeTabulkyLite(navController: NavController) {
@@ -16,7 +17,8 @@ fun NavigationIconTopAppBarKalorickeTabulkyLite(navController: NavController) {
     ) {
         Icon(
             imageVector = Icons.Default.ArrowBack,
-            contentDescription = null
+            contentDescription = null,
+            tint = KalorickeTabulkyLiteTheme.colors.onSurface
         )
     }
 }
